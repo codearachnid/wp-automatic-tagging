0e87d4d71d3b0d08a669b8bb19e82e69511f4686
Timothy Wood <tim@imaginesimplicity.com>
2012-06-13 12:10:15 -0400

    init plugin code + tagthenet api setup

---
+++ libs/api.tagthenet.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+API Access to query tags from http://www.tagthe.net please use responsibly.
+
+*/
+
+
+// Don't load directly
+if ( !defined('ABSPATH') ) { die('-1'); }
+
--- wp-autotagging.php
+++ wp-autotagging.php
@@ -29,3 +29,34 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // Don't load directly
 if ( !defined('ABSPATH') ) { die('-1'); }
+
+if( !class_exists('wp_auto_tagging') ) {
+	class wp_auto_tagging {
+		private function __construct() {
+
+		}
+		public function retrieve_content( $args ) {
+			$defaults = array(
+				'post_type'			=> 'post',
+				'posts_per_page'	=> -1
+				// 'meta_query' 		=> array(
+				// 							array(
+				// 								'key' 		=> '_wp_auto_tagged',
+				// 								'value' 	=> '',
+				// 								'compare' 	=> 'NOT LIKE'
+				// 							)
+				// 						)
+				);
+			$args = wp_parse_args( $args, $defaults);
+			$wp_query = new WP_Query( $args );
+
+			foreach( $wp_query->posts as $post ) {
+				$title = $post->post_title;
+				$content = strip_shortcodes( $post->post_content );
+			}
+		}
+		public function load_api() {
+			
+		}
+	}
+}
